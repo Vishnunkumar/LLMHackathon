1c65b6aece617d9ec0e04afd889be50764c8d059
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-09-11 19:06:06 +0530

    Update Home.py

--- src/Home.py
+++ src/Home.py
@@ -4,13 +4,18 @@ warnings.filterwarnings('ignore')
 import os
 import numpy as np
 import streamlit as st
+import pandas as pd
 from langchain.embeddings import HuggingFaceEmbeddings
 from sklearn.metrics import DistanceMetric
+from langchain.llms import HuggingFaceEndpoint, HuggingFaceHub, HuggingFacePipeline
+from langchain import PromptTemplate, LLMChain
 
 st.title(":blue[HR Buddy]")
-st.subheader("Job Scorer")
+st.text("A space for HR to analyze and optimize the selection process")
 embeddings = HuggingFaceEmbeddings()
 dist = DistanceMetric.get_metric('euclidean')
+repo_id = "tiiuae/falcon-7b"
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
 
 def score_job_tile(job_desc, job_title):
     """
@@ -31,9 +36,48 @@ def score_job_tile(job_desc, job_title):
 
 job_desc = st.text_input("Enter Job description")
 job_title = st.text_input("Enter Job Title")
+submit_one = st.button("Get comparison of the title and description")
 
 # The code block `if job_desc and job_title:` checks if both `job_desc` and `job_title` variables have
 # values. If both variables have values (i.e., they are not empty), it executes the following code:
-if job_desc and job_title:
-    st.markdown("_The distance is:_")
-    st.success(str(score_job_tile(job_desc, job_title)))
+if submit_one:
+    if score_job_tile(job_desc, job_title) > 1.2:
+        st.error("Not Matching")
+    else:
+        st.success("Matching")
+
+title_submit = st.button("Get a proper Job Title")
+if title_submit:
+    template = """Prompt: {question} Sure here is the best job title for the given job description"""
+    question = "Generate a proper job title for the given job description: {0}".format(job_desc)
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm = HuggingFaceHub(
+        repo_id=repo_id, model_kwargs={"temperature": 0.5, "top-k": 50, "top-p":.85, "min_new_tokens": 1024}
+        )
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    text = llm_chain.run(question)
+    text_list = [x for x in text.split("\n") if len(x) > 2]
+    templatedf = pd.DataFrame(text_list)
+    templatedf.columns = ['Job titles']
+    st.markdown("**Some suggested job titles**")
+    st.dataframe(templatedf)
+
+job_submit = st.button("Generate alternative Job descriptions")
+if job_submit:
+    template = """Prompt: {question} Sure here is an alternate version"""
+    question = "Provide an alternate job description for the provided description {0}".format(job_desc)
+
+    k = 0
+    while k < 5:
+        prompt = PromptTemplate(template=template, input_variables=["question"])
+        llm = HuggingFaceHub(
+            repo_id=repo_id, model_kwargs={"temperature": 0.5, "top-k": 50, "top-p":.85, "min_new_tokens": 1024}
+        )
+        llm_chain = LLMChain(prompt=prompt, llm=llm)
+        text = llm_chain.run(question)
+        template = template + "\n" + text  
+        k = k + 1
+    st.markdown("_Alernate Job description:_")
+    st.code(template, language='textile')
+
