bafcfe1e53449b7f78aafdc1c4755daa33a4e442
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2023-09-15 14:23:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
+__import__('pysqlite3')
+
 import warnings
 warnings.filterwarnings('ignore')
 
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import os
+import streamlit as st
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader, PyPDFLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.llms import HuggingFaceEndpoint, HuggingFaceHub, HuggingFacePipeline
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
+from tempfile import NamedTemporaryFile 
 
-st.title(":blue[ChatDF]")
-st.text("Ask anything from the PDF shared and Voila! the bot will answer")
+st.title("Streamlit Hackathon 2023")
+st.subheader(":purple[QA-PDF]",divider="rainbow")
+st.markdown("**A bot to answer questions from the uploaded PDF document**")
 
 os.environ['HUGGINGFACEHUB_API_TOKEN'] = "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"
 embeddings = HuggingFaceEmbeddings()
 repo_id = "tiiuae/falcon-7b-instruct"
-llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"top-k": 50, "top-p":.90, "min_new_tokens": 256})
+llm = HuggingFaceHub(repo_id=repo_id)
 
-uploaded_file = st.file_uploader("Choose a file", "pdf")
+uploaded_file = st.file_uploader("Upload PDF", "pdf")
 if uploaded_file is not None:
-    loader = PyPDFLoader("input/samplepdf/Vishnu_Nandakumar_Dev.pdf")
-    pages = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(pages)
-    embeddings = HuggingFaceEmbeddings()
-    docsearch = Chroma.from_documents(texts, embeddings)
+    with st.spinner('Please wait for the bot to respond.'):
+        bytes_data = uploaded_file.read()
+        with NamedTemporaryFile(delete=False) as tmp:
+            tmp.write(bytes_data)
+        loader = PyPDFLoader(tmp.name)
+        pages = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(pages)
+        embeddings = HuggingFaceEmbeddings()
+        docsearch = Chroma.from_documents(texts, embeddings)
+        
+        prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Also try to be subjective on the answer and don't include unwanted text
+        {context}
+        Question: {question}
+        Briefly Answer:"""
 
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
-    st.text(qa.run(query))
+        PROMPT = PromptTemplate(
+            template=prompt_template, input_variables=["context", "question"]
+        )
+        chain_type_kwargs = {"prompt": PROMPT}
+    st.success("Model and Embedding are loaded")
+
+    query = st.text_input("Ask your Question")
+    if query:
+        qa = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
+        st.subheader("Answer")
+        answer = qa.run(query).split("\n")[0]
+        st.info(answer)
+
+    os.remove(tmp.name)
